ddc5ab6507124d4a15a80c86db1b0d62ef364465
Aleksandra Knott <aleksandraknott@gmail.com>
2024-09-04 09:23:48 +0000

    post request with stringfy send productId and act as Json object

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -17,4 +17,20 @@ for (i = 0; i < updateBtns.length; i++) {
 
 function updateUserOrder(productId, action){
     console.log('User is authenticated, sending data...')
+
+    var url = '/update_item/'
+
+    fetch(url, {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+        },
+        body:JSON.stringify({'productId':productId, 'action':action})
+    })
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log('Data:', data)
+    });
 }
